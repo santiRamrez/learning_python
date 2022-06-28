56ef1b7a1c0f53bf3e7d960f40fcdd4034542061
Santiago Ramírez <sarmejias@gmail.com>
2022-06-28 19:28:07 -0400

    Create suma.py

---
+++ suma.py
@@ -0,0 +1,10 @@
+def suma_lateral():
+    imp:str = input("Ingrese un número: ")
+    num:int = int(imp)
+    cont:int = 1
+    top:int = 10
+    while (cont <= top):
+        print(num * cont)
+        cont = cont + 1
+
+suma_lateral()
